bc1e2185a2f435ba5735055ef49473b31b02364d
irmakko <147623423+irmakko@users.noreply.github.com>
2023-10-12 15:35:50 +0300

    Add files via upload

---
+++ csharp/odev04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tek = new List<int>();
+            List<int> cift = new List<int>();
+            for (int i=0;  i<=10; i++)
+            {
+                if(i %2==0)
+                {
+                    cift.Add(i);
+                }
+                else
+                {
+                    tek.Add(i);
+                }
+            }
+            foreach(int sayilar in tek)
+            {
+                Console.WriteLine(sayilar);
+            }
+            foreach (int sayilar in cift)
+            {
+                Console.WriteLine(sayilar);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
