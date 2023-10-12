2f0fecf181160b592c9c609655906c3dd891597f
irmakko <147623423+irmakko@users.noreply.github.com>
2023-10-12 15:28:12 +0300

    Add files via upload

---
+++ csharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ucgen(5);
+            ucgen(5,'-');
+            
+        }
+        public static void ucgen(int a)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                for (int c = 0; c < i; c++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            
+
+
+        }
+        public static void ucgen(int a, char b)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                for (int c = 0; c < i; c++)
+                {
+                    Console.Write(b);
+                }
+
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
