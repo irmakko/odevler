c23d03312816e608be81405b91ea9e31ba1bccba
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-10-18 15:42:04 +0300

    Add files via upload

---
+++ python/odev07.py
@@ -0,0 +1,12 @@
+
+        
+class kimlik:
+  def __init__(self, ad, soyad,cinsiyet):
+    self.ad = ad
+    self.soyad = soyad
+    self.cinsiyet = cinsiyet
+p1 = kimlik("ırmak","koç", "kadın")
+
+print(p1.ad)
+print(p1.soyad)
+print(p1.cinsiyet)
