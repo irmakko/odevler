7597b2a49628aecef7415650b7b0f981272ab340
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-10-18 17:03:31 +0300

    Add files via upload

---
+++ csharpornekler/ucgeninalani.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucgenalanı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alan, yukseklik, kenar;
+            Console.WriteLine("üçgenin kenar uzunluğunu gieriniz: ");
+            kenar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("üçgenin yüksekliğini giriniz: ");
+            yukseklik = Convert.ToInt32(Console.ReadLine());
+            alan = kenar * yukseklik / 2;
+            Console.WriteLine("üçgenin alanı: "+alan);
+            Console.ReadLine();
+
+        }
+    }
+}
