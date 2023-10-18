e813b51d055ce3d7ebfc80604712a0b97308c317
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-10-18 17:11:10 +0300

    Add files via upload

---
+++ csharpornekler/alancevrehesaplama.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alancevrehesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kisakenar, uzunkenar, alan, cevre;
+            Console.WriteLine("kısa kenarı girin: ");
+            kisakenar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("uzun kenarı girin: ");
+            uzunkenar = Convert.ToInt32(Console.ReadLine());
+            alan = kisakenar * uzunkenar;
+            cevre = (kisakenar + uzunkenar) * 2;
+            Console.WriteLine("alan: "+alan);
+            Console.WriteLine("cevre"+cevre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharpornekler/yuzdehesaplama.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basityüzdehesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi, yuzde, sonuc;
+            Console.WriteLine("sayıyı giriniz: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("yuzdeyi giriniz: ");
+            yuzde = Convert.ToDouble(Console.ReadLine());
+            sonuc = sayi * yuzde / 100;
+            Console.WriteLine("girilen sayının % {0}'i:{1}",yuzde,sonuc);
+            Console.ReadKey();
+        }
+    }
+}
