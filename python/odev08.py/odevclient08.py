41b05dc9d063976240d2484ab6ed2ecc514d982b
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-10-26 14:30:00 +0300

    Add files via upload

---
+++ python/odev08.py/odevclient08.py
@@ -0,0 +1,20 @@
+import socket 
+import datetime
+host = socket.gethostname()  
+port = 5000  
+
+client_socket = socket.socket()  
+client_socket.connect((host, port))  
+def sendmsg(text):
+        client_socket.send(text.encode())
+        
+
+sendmsg("Merhaba Dünya!")
+     
+    
+   
+
+    
+    
+
+  
