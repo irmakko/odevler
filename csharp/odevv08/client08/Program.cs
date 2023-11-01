11904d25740a936e2d116b3739861ef2202a8978
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-11-01 17:50:52 +0300

    Add files via upload

---
+++ csharp/odevv08/client08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client08
+{
+    class UDPClient
+    {
+        static void Main(string[] args)
+        {
+            
+            sendmsg("Hello world");
+        }
+
+       
+
+        public static void sendmsg(string message)
+        {
+             UdpClient udpClient = new UdpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback,12345);
+            
+            byte[] sendData = Encoding.UTF8.GetBytes(message);
+            udpClient.Send(sendData, sendData.Length, serverEndPoint);
+        }
+       
+    }
+}
