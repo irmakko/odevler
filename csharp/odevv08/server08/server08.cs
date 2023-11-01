6da6df81419aefc4d6fe6638dc803dd2cdf9e57f
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-11-01 17:57:48 +0300

    Add files via upload

---
+++ csharp/odevv08/server08/server08.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server08
+{
+    class UDPServer
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpServer = new UdpClient(12345);//dinlemeye basladı.
+            Console.WriteLine("Dinliyor...");
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                byte[] receivedData = udpServer.Receive(ref clientEndPoint);
+                string receivedText = Encoding.UTF8.GetString(receivedData);
+                string responseText = receivedText + "(" + DateTime.Now.ToString("HH:mm") + ")";
+                byte[] responseData = Encoding.UTF8.GetBytes(responseText);
+                udpServer.Send(responseData, responseData.Length, clientEndPoint);
+                Console.WriteLine("Alındı: "+responseText);
+            }
+
+
+
+        }
+    }
+}
+
+
