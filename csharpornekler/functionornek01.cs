a26d63be82f34bcb749095654a8c7dab7e352dbc
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2023-11-02 16:17:25 +0300

    Add files via upload

---
+++ csharpornekler/functionornek01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orn
+{
+    class Program
+    {
+
+
+        static void hello(string fname)
+        {
+
+            Console.WriteLine("merhaba "+fname);
+        }
+        static void hello(string fname,string sname)
+        {
+
+            Console.WriteLine("merhaba"+" "+fname+" "+sname);
+        }
+        static void hello(string a,string fname, string sname, int yas)
+        {
+            Console.WriteLine(a+fname + sname + yas);
+
+        }
+        static void Main(string[] args)
+        {
+            hello("merhaba \n", "ırmak \n", "koc \n",16);
+            hello("ırmak","koc");
+
+            Console.ReadLine();
+        }
+    }
+}
