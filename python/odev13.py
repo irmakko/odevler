433145c555e9ff8db5ad80ba0be41a56c2d7fc8b
ırmak koç <147623423+irmakkoc34@users.noreply.github.com>
2024-02-23 09:56:19 +0300

    Update odev13.py

--- python/odev13.py
+++ python/odev13.py
@@ -10,15 +10,23 @@ def main():
     ana_oge_a=tree.insert("","end",text="A",open=False)
     ana_oge_b=tree.insert("","end",text="B",open=False)
     #A1,A2,A3 ALT CHECKBOX LARI OLUŞTURALIM
-    a1=tree.insert(ana_oge_a,"end",text="A1",tags=("A",))
-    a2=tree.insert(ana_oge_a,"end",text="A2",tags=("A",))
-    a3=tree.insert(ana_oge_a,"end",text="A3",tags=("A",))
+    a1=tree.insert(ana_oge_a,"end",text="A1",tags=("A",),open=False)
+    a2=tree.insert(ana_oge_a,"end",text="A2",tags=("A",),open=False)
+    a3=tree.insert(ana_oge_a,"end",text="A3",tags=("A",),open=False)
+    #A1,A2,A3'ÜNDE ALT CHECKBOXLARINI OLUŞTURALIM
+    a1_1=tree.insert(a1,"end",text="A1.1",tags=("A",))
+    a2_2=tree.insert(a2,"end",text="A2.2",tags=("A",))
+    a3_3=tree.insert(a3,"end",text="A3.3",tags=("A",))
     #B1,B2,B3 ALT CHECKBOXLARI OLUŞTURALIM
-    b1=tree.insert(ana_oge_b,"end",text="B1",tags=("B",))
-    b2=tree.insert(ana_oge_b,"end",text="B2",tags=("B",))
-    a3=tree.insert(ana_oge_b,"end",text="B3",tags=("B",))
+    b1=tree.insert(ana_oge_b,"end",text="B1",tags=("B",),open=False)
+    b2=tree.insert(ana_oge_b,"end",text="B2",tags=("B",),open=False)
+    b3=tree.insert(ana_oge_b,"end",text="B3",tags=("B",),open=False)
+    #B1,B2,B3'NDE ALT CHECKBOXLARINI OLUŞTURALIM
+    b1_2=tree.insert(b1,"end",text="B1.1",tags=("B",))
+    b2_2=tree.insert(b2,"end",text="B2.2",tags=("B",))
+    b3_3=tree.insert(b3,"end",text="B3.3",tags=("B",))
 
     pencere.mainloop()
 
-if __name__== "__main__":
+if __name__== "__main__":  
     main()
